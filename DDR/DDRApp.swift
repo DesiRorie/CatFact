0509d070ac8ddaf75d051dd237ef724b13ed088d
DesiRorie <desirorie@icloud.com>
2023-06-22 23:09:24 -0400

    Initial Commit

---
+++ DDR/DDRApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDRApp.swift
+//  DDR
+//
+//  Created by Desi Rorie on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DDRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
